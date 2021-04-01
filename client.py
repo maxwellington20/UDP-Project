8750899bdf1f273b132eaec46a98acdb2c569813
maxwellington20 <57428636+maxwellington20@users.noreply.github.com>
2021-04-01 17:08:01 -0500

    Server Ack Print
    Cleaned up the code a bit and added a method to print an ack before it sends

--- client.py
+++ client.py
@@ -6,20 +6,39 @@ seq_num = 5895
 addr = "192.168.1.173"
 port_num = 6789
 pos = "14 S 368052 3899189"
-vel = 110.99
+vel = 110.0
+time = 0.2
 accel = 1.38
 brk_ctrl = 0.0
 throttle = 0.46
 
-#prepare packet data
-s = str(seq_num) + "," + str(addr) + "," + str(pos) + "," + str(vel)
-s += "," + str(accel) + "," + str(brk_ctrl) + "," + str(throttle)
+#print initial values
+print("Initial GPS Position: %s" %(pos))
+print("Initial Velocity: %.1fkm/h" %(vel))
+print("Time Interval: %.1fs" %(time))
+print("============================================")
 
-#print(s)
-data = str.encode(s)
+#prepare client_pack data
+def createPacket():
+    client_pack = str(seq_num) + "," + str(addr) + "," + str(pos) + "," + str(vel)
+    client_pack += "," + str(accel) + "," + str(brk_ctrl) + "," + str(throttle) + ","
+    return str.encode(client_pack) #convert list of variables to bytes
 
+#print packet info
+def printPacketSent():
+    print("Packet Sent:")
+    print("Sequence No. = %d" %(seq_num))
+    print("IP = %s" %(addr))
+    print("GPS Position = %s" %(pos))
+    print("Velocity = %.2f" %(vel))
+    print("Acceleration = %.2f" %(accel))
+    print("Brake Control = %.1f" %(brk_ctrl))
+    print("Gas Throttle = %.2f" %(throttle))
+
+#connect to server
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#send packet
+#send client_pack
+data = createPacket()
 clientSock.sendto(data, (addr, port_num))
-print("Packet Sent:")
+printPacketSent()
--- server.py
+++ server.py
@@ -1,18 +1,119 @@
 import socket
 #import struct
 
-serv_addr = "192.168.1.173"
-serv_port = 6789
+#initial server values
+addr = "192.168.1.173"
+port = 6789
+pos = "14 S 368058 3899192"
+vel = 110.0
+time = 0.2
 
+#print initial values
+print("Initial GPS Position: %s" %(pos))
+print("Initial Velocity: %.1fkm/h" %(vel))
+print("Time Interval: %.1fs" %(time))
+print("============================================")
+
+#create socket
 servSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-servSock.bind((serv_addr, serv_port))
+#connect to client
+servSock.bind((addr, port))
+
+#takes variable list and conjoins into a string
+def getVarString(x):
+    s1 = ""
+    return(s1.join(x))
+
+#extract values from packet and print results
+def printPacketStats(s):
+    print("Packet Received:")
+    i = 0
+    arr = list(s)
+
+    # I originally wanted the following lines to be contained
+    # in one big while loop, but I kept getting IndexOutOfBounds 
+    # and ran out of time to solve the issue :(
+
+    #first is sequence number
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    seq_num = getVarString(temp)
+    i = i + 1
+    print("Sequence No. = %s" %(seq_num))
+
+    #second is client ip address
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_ip = getVarString(temp)
+    i = i + 1
+    print("IP = %s" %(client_ip))
+
+    #third is client position
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_pos = getVarString(temp)
+    i = i + 1
+    print("GPS Position = %s" %(client_pos))
+    
+    #fourth is client velocity
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_velocity = getVarString(temp)
+    i = i + 1
+    print("Velocity = %s" %(client_velocity))
+
+    #fifth is client acceleration
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_accel = getVarString(temp)
+    i = i + 1
+    print("Acceleration = %s" %(client_accel))
+
+    #sixth is client brake control
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_bc = getVarString(temp)
+    i = i + 1
+    print("Brake Control = %s" %(client_bc))
+
+    #lastly is client gas throttle
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_gt = getVarString(temp)
+    i = i + 1
+    print("Gas Throttle = %s\n" %(client_gt))
+
+    return seq_num #this is for ack
+    
+def printPacketSent(num):
+    print("Ack Packet Sent:")
+    print("Type = Ack")
+    print("Sequence No. = %s\n" %(num))
+
 
 while True:
-    print("Server Listening...")
-
+    #receive packet from client
     data, addr = servSock.recvfrom(1024)
+    client_pack = bytes.decode(data)
 
-    s = bytes.decode(data)
+    ack_num = printPacketStats(client_pack)
+    printPacketSent(ack_num)
 
-    print(s)
+
+
+
